40c4af551550ecd37c4f602b0c554c139788a7f5
Fernando <fernando.inversionmf@gmail.com>
2024-09-02 20:59:48 -0700

    Update imges in folder public

--- components/Avatar2.js
+++ components/Avatar2.js
@@ -3,8 +3,8 @@ import Image from "next/image";
 const Avatar2 = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
-      <img
-        src={"/public/img/avatar2.png"}
+      <image
+        src={"/avatar2.png"}
         width={737}
         height={678}
         alt="avatar"

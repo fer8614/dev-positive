6c692eb50c26cdb34185da8ba6475068f935ced1
Fernando <fernando.inversionmf@gmail.com>
2024-09-02 20:29:50 -0700

    Update img avatar 2

--- components/Avatar2.js
+++ components/Avatar2.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Avatar2 = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
-      <Image
+      <img
         src={"/img/Avatar2.png"}
         width={737}
         height={678}

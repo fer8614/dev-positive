5b387961e3244f259ddd04add8b6c7bbb988dcd7
Fernando <fernando.inversionmf@gmail.com>
2024-09-02 19:56:54 -0700

    Update image avatar2

--- components/Avatar2.js
+++ components/Avatar2.js
@@ -4,7 +4,7 @@ const Avatar2 = () => {
   return (
     <div className="hidden xl:flex xl:max-w-none">
       <Image
-        src={"/avatar2.png"}
+        src={"/public/avatar2.png"}
         width={737}
         height={678}
         alt="avatar"
